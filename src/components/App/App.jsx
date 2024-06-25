db1aa0f21b687c4e32874c4cc6ba1c6a06505b9c
Max-IT-ZT <babuskomax@gmail.com>
2024-06-25 21:49:49 +0300

    finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,47 @@
+import { useEffect, useState } from "react";
+import baseContact from "../../data/baseContact.json";
+import { FaAddressBook } from "react-icons/fa";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactForm from "../ContactForm/ContactForm";
+import SearchFalse from "../SearchFalse/SearchFalse";
+import css from "./App.module.css";
+const getInitialContact = () => {
+  const saveContact = localStorage.getItem("contacts");
+  return saveContact ? JSON.parse(saveContact) : baseContact;
+};
+
 export default function App() {
+  const [contacts, setContacts] = useState(getInitialContact);
+  const [filter, setFilter] = useState("");
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContacts = (newContact) => {
+    setContacts((prevContact) => {
+      return [...prevContact, newContact];
+    });
+  };
+  const deleteContact = (contactId) => {
+    setContacts((prevContact) => {
+      return prevContact.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const visibleContact = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div>
-      <h1>Phonebook</h1>
-      {/* <ContactForm /> */}
-      {/* <SearchBox /> */}
-      {/* <ContactList /> */}
+    <div className={css.container}>
+      <h1 className={css.title}>
+        Phonebook <FaAddressBook />
+      </h1>
+      <ContactForm onAdd={addContacts} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContact} deleteContact={deleteContact} />
+      {visibleContact < 1 && <SearchFalse />}
     </div>
   );
 }
